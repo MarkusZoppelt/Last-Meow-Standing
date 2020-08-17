8d40ef7d592341b7a3d88e1a9f138d4a6f9ec18b
Tias <iaroslav.titov@gmail.com>
2020-08-17 10:50:15 -0600

    Pause menu added

---
+++ Assets/Scripts/GUI/GameOverScreen/PauseScreenController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreenController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseScreen;
+    private bool paused;
+
+    public void Pause()
+    {
+        paused = true;
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        paused = false;
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paused)
+        {
+            if (Input.anyKeyDown)
+            {
+                Resume();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Pause"))
+            {
+                Pause();
+            }
+        }
+    }
+}
